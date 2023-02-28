7f7031e49b5891eaa6a24e5a9c48f27d1f9a56ad
Lokesh Singh <ashawat.lokesh@gmail.com>
2023-02-28 12:38:08 +0530

    added a method in controller class

--- src/main/java/com/lokesh/webfluxdemo/controller/AnimalController.java
+++ src/main/java/com/lokesh/webfluxdemo/controller/AnimalController.java
@@ -1,14 +1,15 @@
 package com.lokesh.webfluxdemo.controller;
 
+import com.lokesh.webfluxdemo.config.MongoConfig;
 import com.lokesh.webfluxdemo.model.Animal;
 import com.lokesh.webfluxdemo.repository.AnimalRepository;
 import com.lokesh.webfluxdemo.resource.AnimalResource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
@@ -19,9 +20,18 @@ public class AnimalController {
     private AnimalRepository animalRepository;
 
     @GetMapping(produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
     public Flux<AnimalResource> findAll() {
         return animalRepository.findAll()
                 .map(AnimalResource::new)
                 .delayElements(Duration.ofSeconds(1L));
+    }
+
+    @GetMapping(value = "/{id}")
+//    @ResponseStatus(HttpStatus.OK)
+    public Mono<AnimalResource> findAnimalById(@PathVariable("id") String id) {
+        return animalRepository.findById(id)
+                .map(AnimalResource::new)
+                .delayElement(Duration.ofSeconds(1L));
     }
 }
